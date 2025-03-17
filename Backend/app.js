de7923faf14a9b047d1e5d2508f14e49d36a85ed
Jiten Kumar pal <jitenkumarpal2017@gmail.com>
2025-03-17 16:47:57 +0530

    DB changes and Jso file changes

--- Backend/app.js
+++ Backend/app.js
@@ -3,6 +3,9 @@ dotenv.config();  //Env var. Configured
 const express = require('express'); //
 const cors = require('cors'); //Set the cors
 const app = express(); //Created a variable
+const connectToDb =  require('./db/db');
+
+connectToDb();
 
 app.use(cors());  //Setup the cors   ,,, In production ntime Jo hame Domain milega Wsise request bkarna wske elabha Sari cheese ko karna block kardena.
 
